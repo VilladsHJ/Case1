3d44106bfacb8a3e4401d0ca198b03abd29c0f6e
VilladsHJ <Villads.joergensen@gmail.com>
2025-08-23 21:47:09 +0200

    c

--- load.py
+++ load.py
@@ -1,3 +1,24 @@
-print("hello world")
-import os, sys
-print(os.path.dirname(sys.executable))
+import requests
+import pandas as pd
+import json
+import parquet as pq
+import pyarrow as pa
+
+# URL til en API-endpoint
+url = "https://api.energidataservice.dk/dataset/Elspotprices"
+
+response = requests.get(url)  # sender en GET-forespørgsel
+data = response.json()        # konverterer svaret til en Python dict
+
+#for key in data["records"]:
+#    print(key)
+column_names = [] #['HourUTC', 'HourDK', 'PriceArea', 'SpotPriceDKK', 'SpotPriceEUR']
+for names in data["records"][0]:
+    column_names.append(names)
+df_elspotprices = pd.DataFrame(data["records"][:])
+
+pa_elspotprices = pa.Table.from_pandas(df_elspotprices)
+        
+
+print(df_elspotprices)
+print(pa_elspotprices)
