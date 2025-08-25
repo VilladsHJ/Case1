41d98307b5cf08153cb0a2a421ec9d2807fb60c3
VilladsHJ <Villads.joergensen@gmail.com>
2025-08-25 11:47:12 +0200

    changed structure to run scripts as functions in main.py

--- analysis.py
+++ analysis.py
@@ -9,43 +9,45 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import plotly.graph_objects as go
 
-filepath = "~/github.com/VilladsHJ/Case1/elspotprices_2025-08-24" # path to the spot prices from the repository
-elspotprices = pq.read_table(filepath)  # saving data to data a pyarrow data frame
 
-df_elspotprices = elspotprices.to_pandas() # Converting to pandas data frame
+def analysis_script():
+    todays_date = dt.date.today()
 
-average_prices = df_elspotprices.groupby("HourUTC")["SpotPriceDKK"].mean().round() # Finding average of spotprices (rounds to nearest whole number)
+    filepath = f"~/github.com/VilladsHJ/Case1/elspotprices_{todays_date}" # path to the spot prices from the repository
+    elspotprices = pq.read_table(filepath)  # saving data to data a pyarrow data frame
 
-unique_hours = list(df_elspotprices.groupby("HourUTC")["HourDK"].indices.keys()) # Finds each unique hour
+    df_elspotprices = elspotprices.to_pandas() # Converting to pandas data frame
 
-df_average_prices = average_prices.to_frame() # Convert to Pandas dataframe
+    average_prices = df_elspotprices.groupby("HourUTC")["SpotPriceDKK"].mean().round() # Finding average of spotprices (rounds to nearest whole number)
 
-list_average_prices = df_average_prices["SpotPriceDKK"].to_list() # Converts prices to a list - This list will be used i
+    unique_hours = list(df_elspotprices.groupby("HourUTC")["HourDK"].indices.keys()) # Finds each unique hour
 
+    df_average_prices = average_prices.to_frame() # Convert to Pandas dataframe
 
-dict_average = {"HourUTC": unique_hours,
-                'AverageSpotPricesDKK': list_average_prices} # Create dictionary for dataframe with only hour and average spot price
-
-
-df_average = pd.DataFrame(dict_average) # Creates pandas dataframe with HourUTC and Average prices from dictionary
+    list_average_prices = df_average_prices["SpotPriceDKK"].to_list() # Converts prices to a list - This list will be used i
 
 
-# The for loop below goes through each unique timestamp and loops through all timestamps in df_elspotprices. 
-#   If there is a match on time stamp each spot price for the corresponding time stamp is compared to the average spotprice for that timestamp. 
-#   If the spot price is lower that the average spot a boolean value is stored (TRUE/FALSE) in the list "list_below_avg"
-list_below_avg = []
-list_avg = []
-for q in range(len(df_average["HourUTC"])): # Loop through unique timestamp
-    for i in range(len(df_elspotprices["HourUTC"])-1,-1,-1): # Loop through timestamps for all spotprices - added steps to range() to reverse the order to ensure data is stored correctly 
-        if df_average["HourUTC"][q] == df_elspotprices["HourUTC"][i]: # check if timestamp is equal
-            list_below_avg.append(df_average["AverageSpotPricesDKK"][q]>df_elspotprices["SpotPriceDKK"][i]) # append Boolean ("true" if spot price is below average) 
-            list_avg.append(df_average["AverageSpotPricesDKK"][q])
-
-df_elspotprices.insert(len(df_elspotprices.keys()), "AverageSpotPricesDKK", list_avg[::-1]) # Inserting column "AverageSpotPricesDKK" into dataframe
-df_elspotprices.insert(len(df_elspotprices.keys()), "IsBelowAvg", list_below_avg[::-1]) # Inserting column "IsBelowAvg" into dataframe
+    dict_average = {"HourUTC": unique_hours,
+                    'AverageSpotPricesDKK': list_average_prices} # Create dictionary for dataframe with only hour and average spot price
 
 
-print(df_elspotprices.to_string())
+    df_average = pd.DataFrame(dict_average) # Creates pandas dataframe with HourUTC and Average prices from dictionary
 
+
+    # The for loop below goes through each unique timestamp and loops through all timestamps in df_elspotprices. 
+    #   If there is a match on time stamp each spot price for the corresponding time stamp is compared to the average spotprice for that timestamp. 
+    #   If the spot price is lower that the average spot a boolean value is stored (TRUE/FALSE) in the list "list_below_avg"
+    list_below_avg = []
+    list_avg = []
+    for q in range(len(df_average["HourUTC"])): # Loop through unique timestamp
+        for i in range(len(df_elspotprices["HourUTC"])-1,-1,-1): # Loop through timestamps for all spotprices - added steps to range() to reverse the order to ensure data is stored correctly 
+            if df_average["HourUTC"][q] == df_elspotprices["HourUTC"][i]: # check if timestamp is equal
+                list_below_avg.append(df_average["AverageSpotPricesDKK"][q]>df_elspotprices["SpotPriceDKK"][i]) # append Boolean ("true" if spot price is below average) 
+                list_avg.append(df_average["AverageSpotPricesDKK"][q])
+
+    df_elspotprices.insert(len(df_elspotprices.keys()), "AverageSpotPricesDKK", list_avg[::-1]) # Inserting column "AverageSpotPricesDKK" into dataframe
+    df_elspotprices.insert(len(df_elspotprices.keys()), "IsBelowAvg", list_below_avg[::-1]) # Inserting column "IsBelowAvg" into dataframe
+
+    return df_elspotprices
 
 
--- main.py
+++ main.py
@@ -5,28 +5,33 @@ import pyarrow.parquet as pq
 from dash import Dash, html, dcc
 import plotly.express as px
 import plotly.graph_objects as go
-from analysis import df_elspotprices, df_average, list_avg
+import ETL
+from analysis import analysis_script
 
 app = Dash() # initialize Dash app
 
-fig1 = px.bar(df_elspotprices, x="HourUTC", y="SpotPriceDKK", color="PriceArea", barmode="group", hover_data="IsBelowAvg") # First bar plot with grouped prices
-fig2 = px.bar(df_elspotprices, x="HourUTC", y="AverageSpotPricesDKK", color="AverageSpotPricesDKK",barmode="overlay") # Second bar plot with average prices
-#fig3 = px.bar(df_elspotprices, x="HourUTC", y="PriceArea", color="IsBelowAvg", color_discrete_map={True: "Blue"})
-fig1.add_scatter(x=df_elspotprices["HourUTC"], y=df_elspotprices["AverageSpotPricesDKK"], mode="lines", name="Average Spot Price")
+def dashboard():
+    df_elspotprices = analysis_script()
+    fig1 = px.bar(df_elspotprices, x="HourUTC", y="SpotPriceDKK", color="PriceArea", barmode="group", hover_data="IsBelowAvg") # First bar plot with grouped prices
+    fig2 = px.bar(df_elspotprices, x="HourUTC", y="AverageSpotPricesDKK", color="AverageSpotPricesDKK",barmode="overlay") # Second bar plot with average prices
 
-app.layout = html.Div([
-        html.Div([
-            html.H1("Grouped Spot Prices"),
-            html.Div('''Spot prices for each area during the day. Trendline showing the average spot price'''),
-            dcc.Graph(id='Grouped', figure=fig1)
-        ]),
-        html.Div([
-            html.H1("Average Spot Prices"),
-            html.Div('''Average spot prices during the day 
-                     (rounded to nearest whole number)'''),
-            dcc.Graph(id='Average', figure=fig2)]),
+    fig1.add_scatter(x=df_elspotprices["HourUTC"], y=df_elspotprices["AverageSpotPricesDKK"], mode="lines", name="Average Spot Price")
 
-])
+    app.layout = html.Div([
+            html.Div([
+                html.H1("Grouped Spot Prices"),
+                html.Div('''Spot prices for each area during the day. Trendline showing the average spot price'''),
+                dcc.Graph(id='Grouped', figure=fig1)
+            ]),
+            html.Div([
+                html.H1("Average Spot Prices"),
+                html.Div('''Average spot prices during the day 
+                        (rounded to nearest whole number)'''),
+                dcc.Graph(id='Average', figure=fig2)]),
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+    ])
+
+if __name__ == '__main__':
+    ETL.ETL_script()
+    dashboard()
+    #app.run(debug=True)
