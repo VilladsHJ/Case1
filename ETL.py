d1b7d8e4f5c86f78999b86e4941b97bbd8b71ccc
VilladsHJ <Villads.joergensen@gmail.com>
2025-08-25 11:47:52 +0200

    changed structure to run scripts as functions in main.py

--- ETL.py
+++ ETL.py
@@ -6,25 +6,18 @@ import parquet
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-# URL til en API-endpoint
-url = "https://api.energidataservice.dk/dataset/Elspotprices"
+def ETL_script():
+    # URL til en API-endpoint
+    url = "https://api.energidataservice.dk/dataset/Elspotprices"
 
-response = requests.get(url)  # sending a GET request
-data = response.json()        # Converts the reply to a Python dict
+    response = requests.get(url)  # sending a GET request
+    response.raise_for_status() # check status codes
+    data = response.json()        # Converts the reply to a Python dict
 
-df_elspotprices = pd.DataFrame(data["records"][:]) # Saves the data as a Pandas data frame
+    df_elspotprices = pd.DataFrame(data["records"][:]) # Saves the data as a Pandas data frame
 
-pa_elspotprices = pa.Table.from_pandas(df_elspotprices) # Converts Pandas data frame to Parquet
+    pa_elspotprices = pa.Table.from_pandas(df_elspotprices) # Converts Pandas data frame to Parquet
 
-todays_date = dt.date.today()
+    todays_date = dt.date.today()
 
-pq.write_table(pa_elspotprices, f'elspotprices_{todays_date}') # Saves the daily spot prices to the repository
-
-#filepath = "~/github.com/VilladsHJ/Case1/elspotprices"
-#elspotprices = pq.read_table(filepath)
-#print(elspotprices)
-
-
-
-#print(df_elspotprices)
-#print(pa_elspotprices)
+    pq.write_table(pa_elspotprices, f'elspotprices_{todays_date}') # Saves the daily spot prices to the repository
